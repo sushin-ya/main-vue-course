21176978fa62f6217c9bca0b1d7d98fda29bc42d
tsukaryu <drgon8otsuka@gmail.com>
2021-10-12 15:55:09 +0900

    add app

--- src/router.js
+++ src/router.js
@@ -1,17 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachResigtration from './pages/coaches/CoachResigtration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestRecieved from './pages/requests/RequestRecieved.vue';
+import NotFound from './pages/NotFound.vue';
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/coaches' },
-    { path: '/coaches', component: null },
+    { path: '/coaches', component: CoachesList },
     {
       path: '/coaches/:id',
-      component: null,
-      children: [{ path: '/contact', component: null }]
+      component: CoachDetail,
+      children: [{ path: '/contact', component: ContactCoach }]
     },
-    { path: '/register', component: null },
-    { path: '/:notFound(.*)', component: null }
+    { path: '/register', component: CoachResigtration },
+    { path: '/requests', component: RequestRecieved },
+    { path: '/:notFound(.*)', component: NotFound }
   ]
 });
 
