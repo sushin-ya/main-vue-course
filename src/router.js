f7a8bf8614d9090934c0e36d834b5ec4406d97d6
tsukaryu <drgon8otsuka@gmail.com>
2021-10-12 15:45:04 +0900

    add router

--- src/router.js
+++ src/router.js
@@ -1,4 +1,18 @@
-import { createRouter } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
-const router = createRouter();
-  
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', redirect: '/coaches' },
+    { path: '/coaches', component: null },
+    {
+      path: '/coaches/:id',
+      component: null,
+      children: [{ path: '/contact', component: null }]
+    },
+    { path: '/register', component: null },
+    { path: '/:notFound(.*)', component: null }
+  ]
+});
+
+export default router;
