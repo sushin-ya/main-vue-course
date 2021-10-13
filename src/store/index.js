915ec51e26f4468d0e19a18b09c2ef127585ecdd
tsukaryu <drgon8otsuka@gmail.com>
2021-10-13 11:23:35 +0900

    247

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex';
 
 import coachesModule from './modules/coaches.js';
+import requestsModule from './modules/requests.js';
 
 const store = createStore({
   modules: {
-    coaches: coachesModule
+    coaches: coachesModule,
+    requests: requestsModule
   },
   state() {
     return {
