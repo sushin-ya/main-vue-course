0de88a189200edef051e9c03f93091a31f11f31f
tsukaryu <drgon8otsuka@gmail.com>
2021-10-13 15:55:47 +0900

    fix

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -16,7 +16,7 @@ export default {
   actions: {
     async login(context, payload) {
       const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.API_URL}`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.VUE_APP_API_KEY}`,
         {
           method: 'POST',
           body: JSON.stringify({
@@ -47,7 +47,7 @@ export default {
     },
     async signup(context, payload) {
       const response = await fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.API_URL}`,
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.VUE_APP_API_KEY}`,
         {
           method: 'POST',
           body: JSON.stringify({
